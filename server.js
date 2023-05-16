98012ae6d9d7d04b2111699c8e5a42e1de74351d
LimelightsCoder <cory.komaguchi@gmail.com>
2023-05-15 18:02:04 -0700

    updates

--- server.js
+++ server.js
@@ -81,12 +81,11 @@ app.post('/create-checkout-session', async (req, res) => {
 });
 
 // Serve frontend files from Netlify
-const buildPath = path.join(__dirname, '../../bmc_app/client/dist'); // Update the path based on your local directory structure
-app.use(express.static(buildPath));
+app.use(express.static('public'));
 
 // Route handler for all requests
 app.get('*', (req, res) => {
-  res.sendFile(path.join(buildPath, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
