ff6172f51fc552e44039bc8bcc9e8314d4873939
LimelightsCoder <113650511+LimelightsCoder@users.noreply.github.com>
2023-05-18 19:43:21 -0700

    Update server.js

--- server.js
+++ server.js
@@ -66,30 +66,29 @@ app.post('/create-checkout-session', async (req, res) => {
       line_items: lineItems,
       success_url: `${process.env.FRONTEND_URL}/success?email=${email}&items=${items.map(item => item.id).join(',')}&amount={CHECKOUT_SESSION_AMOUNT}`,
       cancel_url: `${process.env.FRONTEND_URL}/registration`,
+
     });
 
-    res.json({ success_url: session.url, email, items, amount: session.amount_total });
-  } catch (e) {
-    res.status(500).json({ error: e.message });
-  }
-});
-
-app.get('/success', async (req, res) => {
-  try {
-    const { email, items, amount } = req.query;
-    // Parse the amount parameter as an integer
-    const amountInCents = parseInt(amount);
-
     // After a successful Stripe purchase, send the order summary email
-    await sendOrderSummaryEmail(email, items, amountInCents);
+    await sendOrderSummaryEmail(email, items, session.amount_total);
 
-    // Redirect the user to a success page
-    res.redirect(`${process.env.FRONTEND_URL}/success`);
+    const updatedSuccessUrl = session.success_url.replace('{CHECKOUT_SESSION_AMOUNT}', session.amount_total);
+    session.success_url = updatedSuccessUrl;
+
+    res.json({ url: session.url });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
 });
 
+// // Serve frontend files
+// const buildPath = path.join(__dirname, '../client/dist');
+// app.use(express.static(buildPath));
+
+// // Route handler for all requests
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(buildPath, 'index.html'));
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
